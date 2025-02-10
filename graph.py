818bbc21633ece0a7ea6a01b099aa4159d0e8d63
Krishnachandran U <kriss52450@gmail.com>
2025-02-11 00:21:22 +0530

    add method to load the graph

--- graph.py
+++ graph.py
@@ -1,30 +1,31 @@
+import graphviz
+import json
 class Graph:
-    def __init__(self, vertices):
-        self.vertices: list[int] = vertices
-        self.adj_list: dict[int, list[int]] = {vertex: [] for vertex in vertices}
+    """ use consecutive natural numbers starting from 0 as vertex labels"""
 
-    def add_edge(self, u, v) -> None:
-        if v not in self.adj_list[u] and u not in self.adj_list[v]:
-            self.adj_list[u].append(v)
-            self.adj_list[v].append(u)
-        else:
-            print(f'Edge {u} - {v} already exists')
+    def __init__(self, filename: str, name: str):
+        with open(f'data/{filename}') as f:
+            data: dict = json.load(f)
+            adjList: dict[str, list[int]] = data[name]
+            self.vertices = list(adjList.keys())
+            self.adjList = {int(k): v for k, v in adjList.items()}
 
-    def remove_edge(self, u, v) -> None:
-        if v in self.adj_list[u] and u in self.adj_list[v]:
-            self.adj_list[u].remove(v)
-            self.adj_list[v].remove(u)
-        else:
-            print(f'No edge between {u} and {v}')
+    def draw(self) -> graphviz.Graph:
+        from config import graphvizConfig, graphvizAttrConfig, graphvizEdgeConfig 
 
-    def get_adj_matrix(self):
-        adj_matrix: list[list[int]] = [
-            [0 for _ in range(len(self.vertices))]
-            for _ in range(len(self.vertices))
-        ]
+        dot = graphviz.Graph(**graphvizConfig)        
+        
+        for vertex in self.vertices:
+            dot.node(str(vertex))
+        
         for u in self.vertices:
-            for v in self.adj_list[u]:
-                adj_matrix[u][v] = 1
+            for v in self.adjList[u]:
+                if u < v:
+                    dot.edge(str(u), str(v), **graphvizEdgeConfig)
 
-    def __str__(self):
-        return str(self.adj_list)
+        try:
+            dot.render(f'img/{id(self)}', format='png', cleanup=True, **graphvizAttrConfig)
+        except Exception as e:
+            print(f'Error rendering graph: {e}')
+
+        return dot
