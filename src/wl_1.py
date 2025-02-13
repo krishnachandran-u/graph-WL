586958afec533e8617995c168e702c1ecb94dff1
Krishnachandran U <kriss52450@gmail.com>
2025-02-13 15:41:18 +0530

    fix issues with wl1 fun

--- src/wl_1.py
+++ src/wl_1.py
@@ -1,8 +1,14 @@
 from src.graph import Graph
 from multiset import * 
 
-def wl_1(g: Graph):
-    colors_prev: dict[int, int] = {v: 0 for v in g.vertices}
+def wl_1(g: Graph) -> list[dict[int, int]]:
+    colors_prev: dict[int, int] = {
+        v: 0 for v in g.vertices
+    }
+
+    colors_stack: list[dict[int, int]] = [colors_prev]
+
+    msets_prev_occ = Multiset([0]*len(g.vertices))
 
     while True: 
         colors_next: dict[int, int] = {}
@@ -18,12 +24,14 @@ def wl_1(g: Graph):
             for v in msets_next[mset]:
                 colors_next[v] = i
 
-        colors_prev_occurrence = Multiset(colors_prev.values())
-        colors_next_occurrence = Multiset(colors_next.values())
+        colors_stack.append(colors_next)
 
-        if colors_prev_occurrence == colors_next_occurrence:
+        msets_next_occ = Multiset([len(v) for v in msets_next.values()])
+
+        if msets_next_occ == msets_prev_occ:
             break
 
         colors_prev = colors_next
+        msets_prev_occ = msets_next_occ
 
-    return colors_next
+    return colors_stack
