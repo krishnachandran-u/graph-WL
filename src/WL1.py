b38b80f7c1d6ab20f4fd49a1c8f86a763a8d5de6
Krishnachandran U <kriss52450@gmail.com>
2025-02-16 17:42:50 +0530

    improve complexity of WL1

--- src/WL1.py
+++ src/WL1.py
@@ -2,28 +2,20 @@ from src.graph import Graph
 import time
 
 def checkWL1(g1: Graph, g2: Graph) -> bool:
-    _, colorsNextOcc1, _ = WL1(g1)
-    _, colorsNextOcc2, _ = WL1(g2)
+    raise NotImplementedError
 
-    return colorsNextOcc1 == colorsNextOcc2 
-
-def WL1(g: Graph, trace: bool = False) -> tuple[list[dict[int, int]], list[int], float] | tuple[dict[int, int], list[int], float]:
+def WL1(g: Graph, trace: bool = False) -> tuple[list[dict[int, int]], float] | tuple[dict[int, int], float]:
     startTime = time.perf_counter()
 
     colorsPrev: dict[int, int] = {v: len(g.neighbors(v)) for v in g.vertices}
+    colorsPrevEnum: dict[int, int] = {color: i for i, color in enumerate(list(dict.fromkeys(colorsPrev.values())))}
+    colorsPrev = {v: colorsPrevEnum[colorsPrev[v]] for v in g.vertices}
 
     if trace:
         colorsStack = [colorsPrev]
 
-    colorsPrevHash: dict[int, int] = {} 
-    for v, color in colorsPrev.items():
-        colorsPrevHash[color] = colorsPrevHash.get(color, 0) + 1
-
-    colorsPrevOcc: list[int] = sorted(list(colorsPrevHash.values()))
-
     while True: 
         colorsNext: dict[int, int] = {}
-        colorsNextHash: dict[int, int] = {}
 
         for v in g.vertices:
             colorNextV: list[int] = [] 
@@ -31,23 +23,22 @@ def WL1(g: Graph, trace: bool = False) -> tuple[list[dict[int, int]], list[int],
                 colorNextV.append(colorsPrev[u])
             colorNextV = tuple(sorted(colorNextV))
             colorsNext[v] = hash(colorNextV)
-            colorsNextHash[colorsNext[v]] = colorsNextHash.get(colorsNext[v], 0) + 1
 
-        colorsNextOcc: list[int] = sorted(list(colorsNextHash.values()))
+        colorsNextEnum: dict[int, int] = {color: i for i, color in enumerate(list(dict.fromkeys(colorsNext.values())))}
+        colorsNext = {v: colorsNextEnum[colorsNext[v]] for v in g.vertices}
         
         if trace:
             colorsStack.append(colorsNext)
-
-        if colorsNextOcc == colorsPrevOcc:
-            break
     
-        colorsPrev = colorsNext
-        colorsPrevOcc = colorsNextOcc
+        if colorsNext == colorsPrev:
+            break
 
-        endTime = time.perf_counter()
-        execTime = endTime - startTime
+        colorsPrev = colorsNext
+
+    endTime = time.perf_counter()
+    execTime = endTime - startTime
         
-        if trace:
-            return colorsStack, colorsNextOcc, execTime
-        else:
-            return colorsNext, colorsNextOcc, execTime
+    if trace:
+        return colorsStack, execTime
+    else:
+        return colorsNext, execTime
